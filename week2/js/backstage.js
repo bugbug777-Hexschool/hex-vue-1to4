48a7b33f96598a84c711bbaa7d37cd5ed4dbe84a
sihle <gubug777@gmail.com>
2022-01-23 16:57:01 +0800

    add week2 Vue work

---
+++ week2/js/backstage.js
@@ -0,0 +1,60 @@
+import {
+  createApp
+} from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.28/vue.esm-browser.min.js';
+const baseUrl = 'https://vue3-course-api.hexschool.io';
+const apiPath = "sihle";
+
+const app = createApp({
+  data() {
+    return {
+      products: {},
+      temp: {}
+    }
+  },
+  methods: {
+    check_login_status() {
+      // Cookie Start
+      // 取得 cookie 中的 token
+      const token = document.cookie.replace(/(?:(?:^|.*;\s*)hexToken\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+      // console.log(`I get the token, ${ token }`);
+      // 將 token 寫入預設
+      axios.defaults.headers.common['Authorization'] = token;
+      // Cookie End
+
+      axios.post(`${ baseUrl }/v2/api/user/check`)
+        .then(res => {
+          this.get_all_products();
+        })
+        .catch(err => {
+          alert("api_path 禁止非本人使用！")
+          window.location = "index.html";
+        })
+    },
+    get_all_products() {
+      axios.get(`${ baseUrl }/v2/api/${ apiPath }/admin/products/all`)
+        .then(res => {
+          this.products = res.data.products;
+        })
+        .catch(err => {
+          // console.log(err.response);
+        })
+    },
+    delete_product(e) {
+      const id = e.target.dataset.id;
+      axios.delete(`${ baseUrl }/v2/api/${ apiPath }/admin/product/${ id }`)
+        .then(res => {
+          delete this.products[id];
+          this.temp = {};
+        })
+        .catch(err => {
+          // console.log(err.response);
+        })
+    }
+
+  },
+  mounted() {
+    this.check_login_status()
+  },
+});
+
+app.mount('#app');
