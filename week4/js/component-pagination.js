404a80dc5b5ef7391ae8ead6b788b23e531539fa
sihle <gubug777@gmail.com>
2022-01-30 00:29:18 +0800

    add: week4 vue work

--- week4/js/component-pagination.js
+++ week4/js/component-pagination.js
@@ -1,17 +1,18 @@
-// v-for with a range
-// https://vuejs.org/v2/guide/list.html#v-for-with-a-Range
-
-export default {
-  props: ['pagination'],
-  template: `<nav aria-label="Page navigation">
-  <ul class="pagination">
-    <li v-if="pagination.has_pre" class="page-item"><a class="page-link" href="#">Previous</a></li>
-    <template v-for="i in pagination.total_pages">
-      <li class="page-item" :class="{active: (pagination.current_page === i)}">
-        <a class="page-link" href="#">{{ i }}</a>
+const pagination = {
+  props: ["pages"],
+  template: `
+  <nav aria-label="Page navigation">
+    <ul class="pagination">
+      <li v-if="pages.has_pre" @click.prevent="$emit('update', pages.current_page-1)" class="page-item"><a class="page-link" href="#">Previous</a></li>
+      <li v-for="i in pages.total_pages" :key="i" class="page-item" :class="{ active: pages.current_page === i}">
+        <a @click.prevent="$emit('update', i)" class="page-link" href="#">{{ i }}</a>
       </li>
-    </template>
-    <li v-if="pagination.has_next" class="page-item"><a class="page-link" href="#">Next</a></li>
-  </ul>
-</nav>`
+      <li v-if="pages.has_next" @click.prevent="$emit('update', pages.current_page+1)" class="page-item"><a class="page-link" href="#">Next</a></li>
+    </ul>
+  </nav>
+  `
+};
+
+export {
+  pagination
 };
